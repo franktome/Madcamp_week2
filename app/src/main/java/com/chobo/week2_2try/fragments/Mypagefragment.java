d5e429c8f440a7ef948705398dc101c1156489d8
franktome <yunhs139@dgist.ac.kr>
2024-01-06 15:57:30 +0900

    <Add function>

--- app/src/main/java/com/chobo/week2_2try/fragments/Mypagefragment.java
+++ app/src/main/java/com/chobo/week2_2try/fragments/Mypagefragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.chobo.week2_2try.MainActivity;
 import com.chobo.week2_2try.R;
+import com.chobo.week2_2try.Real_main;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Mypagefragment extends Fragment {
@@ -22,8 +25,15 @@ public class Mypagefragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_mypage, container, false);
 
+        Button before= view.findViewById(R.id.logout_btn);
+        before.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(requireActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
-
-
 }
